a2286cae9cff6845dcfac11fddddcf717695fbe8
Lantam <leonleonardo07@gmail.com>
2024-07-19 21:14:41 +0200

    Make django use the correct plural form

--- src/backend/inventory/models/category.py
+++ src/backend/inventory/models/category.py
@@ -5,5 +5,8 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
