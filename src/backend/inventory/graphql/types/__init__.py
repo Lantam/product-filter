2986fd10bcd83501e1617d6bdc241bff3a911f11
Lantam <leonleonardo07@gmail.com>
2024-07-19 22:28:14 +0200

    corrected imports

--- src/backend/inventory/graphql/types/__init__.py
+++ src/backend/inventory/graphql/types/__init__.py
@@ -1,3 +1,3 @@
-from backend.inventory.graphql.types.category import CategoryType
+from inventory.graphql.types.category import CategoryType
 from inventory.graphql.types.product import ProductInput, ProductType
-from backend.inventory.graphql.types.supplier import SupplierType
+from inventory.graphql.types.supplier import SupplierType
